c5975d78d5f1f37b31f6e92bac3f52789562e46c
pcplayer <pcplayer@gmail.com>
2024-05-11 19:39:18 +0800

    Demo 里面的调用代码也做了修改

--- Unit2.dfm
+++ Unit2.dfm
@@ -10,36 +10,75 @@ object Form2: TForm2
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
-  OldCreateOrder = False
   OnCreate = FormCreate
   OnDestroy = FormDestroy
-  PixelsPerInch = 96
   TextHeight = 13
-  object BtnSuccess: TButton
-    Left = 176
-    Top = 112
-    Width = 75
-    Height = 25
-    Caption = 'Success'
+  object Panel1: TPanel
+    Left = 0
+    Top = 0
+    Width = 706
+    Height = 89
+    Align = alTop
+    Caption = 'Panel1'
     TabOrder = 0
-    OnClick = BtnSuccessClick
+    object Panel3: TPanel
+      AlignWithMargins = True
+      Left = 4
+      Top = 4
+      Width = 698
+      Height = 81
+      Align = alClient
+      Caption = 'Panel3'
+      TabOrder = 0
+      ExplicitLeft = 192
+      ExplicitTop = 16
+      ExplicitWidth = 185
+      ExplicitHeight = 41
+    end
   end
-  object BtnInfo: TButton
-    Left = 257
-    Top = 112
-    Width = 75
-    Height = 25
-    Caption = 'Info'
+  object Panel2: TPanel
+    Left = 0
+    Top = 89
+    Width = 706
+    Height = 128
+    Align = alTop
+    Caption = 'Panel2'
     TabOrder = 1
-    OnClick = BtnInfoClick
-  end
-  object BtnError: TButton
-    Left = 338
-    Top = 112
-    Width = 75
-    Height = 25
-    Caption = 'Error'
-    TabOrder = 2
-    OnClick = BtnErrorClick
+    object BtnError: TButton
+      Left = 386
+      Top = 103
+      Width = 75
+      Height = 25
+      Caption = 'Error'
+      TabOrder = 0
+      OnClick = BtnErrorClick
+    end
+    object BtnInfo: TButton
+      Left = 305
+      Top = 103
+      Width = 75
+      Height = 25
+      Caption = 'Info'
+      TabOrder = 1
+      OnClick = BtnInfoClick
+    end
+    object BtnSuccess: TButton
+      Left = 224
+      Top = 103
+      Width = 75
+      Height = 25
+      Caption = 'Success'
+      TabOrder = 2
+      OnClick = BtnSuccessClick
+    end
+    object Button1: TButton
+      Left = 600
+      Top = 96
+      Width = 75
+      Height = 25
+      Caption = 'Form1'
+      TabOrder = 3
+      OnClick = Button1Click
+    end
   end
 end
--- Unit2.pas
+++ Unit2.pas
@@ -4,22 +4,28 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, uTToastMessage;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, uTToastMessage,
+  Vcl.ExtCtrls;
 
 type
   TForm2 = class(TForm)
     BtnSuccess: TButton;
     BtnInfo: TButton;
     BtnError: TButton;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Button1: TButton;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure BtnSuccessClick(Sender: TObject);
     procedure BtnInfoClick(Sender: TObject);
     procedure BtnErrorClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
-    var
-      ToastMessage : TToastMessage;
+
+    FPanel: TPanel;
   public
     { Public declarations }
   end;
@@ -29,32 +35,47 @@ var
 
 implementation
 
+uses Unit1;
+
 {$R *.dfm}
 
 procedure TForm2.BtnErrorClick(Sender: TObject);
 begin
-  ToastMessage.Toast(tpError,'Error','My Text');
+  ToastMessage.Toast(Self, tpError,'Error','My Text');
 end;
 
 procedure TForm2.BtnInfoClick(Sender: TObject);
 begin
-  ToastMessage.Toast(tpInfo,'Info','My Text');
+  ToastMessage.Toast(Self, tpInfo,'Info','My Text');
 end;
 
 procedure TForm2.BtnSuccessClick(Sender: TObject);
 begin
-  ToastMessage.Toast(tpSuccess,'Success','My Text');
+  ToastMessage.Toast(Self, tpSuccess,'Success','My Text');
+end;
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  Form1.ShowModal;
 end;
 
 procedure TForm2.FormCreate(Sender: TObject);
 begin
-  ToastMessage := TToastMessage.Create(Self);
+  //ToastMessage := TToastMessage.Create(Self);
+
+  //后创建的就会在 Z 轴方向的更上面一层，则会把 Toast 的 Panel 挡住，显示不了。解决办法：Toast 里面增加一个把自己拉到前面的 BringToFront 操作。
+  FPanel := TPanel.Create(Self);
+  FPanel.Name := 'MyPanel';
+  FPanel.Parent := Self;
+  FPanel.Align := alTop;
+  FPanel.Height := 200;
+  //FPanel.SendToBack;
 end;
 
 procedure TForm2.FormDestroy(Sender: TObject);
 begin
-  if Assigned(ToastMessage) then
-    FreeAndNil(ToastMessage);
+//  if Assigned(ToastMessage) then
+//    FreeAndNil(ToastMessage);
 end;
 
 end.
